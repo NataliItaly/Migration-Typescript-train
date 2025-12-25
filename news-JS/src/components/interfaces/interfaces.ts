42b1cfe247e7052eafd65de0b2df0c90c1aa101d
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-12-25 19:03:01 +0100

    feat: add newsresponse interface

--- news-JS/src/components/interfaces/interfaces.ts
+++ news-JS/src/components/interfaces/interfaces.ts
@@ -8,6 +8,23 @@ export interface Source {
     name: string;
 }
 
+export interface NewsItem {
+    source: Source;
+    author?: string | null;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage?: string | null;
+    publishedAt: string;
+    content?: string;
+}
+
+export interface NewsResponse {
+    status: string;
+    totalResults: number;
+    articles: NewsItem[];
+}
+
 export interface SourcesResponse {
     status: string;
     sources: Source[];
