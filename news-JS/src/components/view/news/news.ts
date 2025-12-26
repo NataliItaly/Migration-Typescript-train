0a2d1806b5b00aa5ee37f59d7d0fe4973c1065d8
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-12-26 12:19:15 +0100

    refactor: add error message if news container doesn't exist

--- news-JS/src/components/view/news/news.ts
+++ news-JS/src/components/view/news/news.ts
@@ -8,7 +8,7 @@ class News {
 
         if (!el) {
             if (options?.required) {
-                console.warn('Container .news not found');
+                throw new Error(`Required element not found: ${selector}`);
             }
         }
         callback(el);
@@ -27,87 +27,41 @@ class News {
         news.forEach((item, idx) => {
             const newsClone = newsItemTemp.content.cloneNode(true) as DocumentFragment;
 
-            /* const newsItemElement = newsClone.querySelector('.news__item') as HTMLElement | null;
-            if (newsItemElement && idx % 2) {
-                newsItemElement.classList.add('alt');
-            } */
             this.setElement(newsClone, '.news__item', (el) => {
                 if (idx % 2) el.classList.add('alt')
             });
 
-            /* const photoElement = newsClone.querySelector('.news__meta-photo') as HTMLElement | null;
-            if (photoElement) {
-                photoElement.style.backgroundImage = `url(${
-                    item.urlToImage || 'img/news_placeholder.jpg'
-                    })`;
-            } */
             this.setElement(newsClone, '.news__meta-photo', (el) => {
                 el.style.backgroundImage = `url(${
                     item.urlToImage || 'img/news_placeholder.jpg'
                     })`
             });
 
-            /* const authorElement = newsClone.querySelector('.news__meta-author') as HTMLElement | null;
-            if (authorElement) {
-                authorElement.textContent = item.author || item.source.name;
-            } */
             this.setElement(newsClone, '.news__meta-author', (el) => {
                 el.textContent = item.author || item.source.name;
             });
 
-            /* const dateElement = newsClone.querySelector('.news__meta-date') as HTMLElement | null;
-            if (dateElement) {
-                dateElement.textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
-            } */
             this.setElement(newsClone, '.news__meta-date', (el) => el.textContent = item.publishedAt
                 .slice(0, 10)
                 .split('-')
                 .reverse()
                 .join('-'));
 
-            /* const titleElement = newsClone.querySelector('.news__description-title') as HTMLElement | null;
-            if (titleElement) {
-                titleElement.textContent = item.title;
-            } */
             this.setElement(newsClone, '.news__description-title', (el) => el.textContent = item.title);
 
-            /* const descriptionSourceElement = newsClone.querySelector('.news__description-source') as HTMLElement | null;
-            if (descriptionSourceElement) {
-                descriptionSourceElement.textContent = item.source.name;
-            } */
             this.setElement(newsClone, '.news__description-source', (el) => el.textContent = item.source.name);
 
-            /* const descriptionElement = newsClone.querySelector('.news__description-content') as HTMLElement | null;
-            if (descriptionElement) {
-                descriptionElement.textContent = item.description;
-            } */
             this.setElement(newsClone, '.news__description-content', (el) => el.textContent = item.description);
 
-            /* const readMoreElement = newsClone.querySelector('.news__read-more a') as HTMLElement | null;
-            if (readMoreElement) {
-                readMoreElement.setAttribute('href', item.url);
-            } */
             this.setElement(newsClone, '.news__read-more a', (el) => el.setAttribute('href', item.url));
 
             fragment.append(newsClone);
         });
 
-        /* const newsContainer = document.querySelector('.news') as HTMLElement | null;
-        if (newsContainer) {
-            newsContainer.innerHTML = '';
-            newsContainer.appendChild(fragment);
-        } else {
-            console.warn('Container .news not found');
-        } */
-
         this.setElement<HTMLElement>(document, '.news', (el) => {
             el.innerHTML = '';
             el.appendChild(fragment);
-        });
+        }, {required: true});
     }
 }
 
