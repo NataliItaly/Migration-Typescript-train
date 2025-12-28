8fb0ccf47f2e98c93dd8c6b4604f4429eeede2f1
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-12-28 01:16:58 +0100

    fix: correct deploy path

--- news-JS/webpack.config.js
+++ news-JS/webpack.config.js
@@ -26,6 +26,7 @@ const baseConfig = {
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
+        publicPath: '/Migration-Typescript-train/',
     },
     plugins: [
         new DotenvWebpackPlugin(),
